385655e83aa312996da8649d07a76e6480e88525
reusdomi <domi_re@web.de>
2022-03-14 21:09:29 +0100

    ProcessData implemented and runtime added to process list

--- ProcessModule.py
+++ ProcessModule.py
@@ -5,8 +5,11 @@ handle operations on processes such as kill() in the future.
 
 # import all necessary libraries and packages
 import psutil
-from datetime import datetime
+from datetime import datetime, date, timedelta
 import pandas as pd
+import os, sys
+import Exceptions
+import numpy as np
 
 
 # Returns an array with all processes and their information
@@ -26,6 +29,11 @@ def get_processes_info():
             if pid == 0:
                 continue
 
+            # Check if process is a system process
+            if os.name == 'nt':
+                if checkSystemProcess(process.name()):
+                    continue
+
             # get process name
             try:
                 name = process.name()
@@ -39,6 +47,8 @@ def get_processes_info():
                 # system processes, using boot time instead
                 create_time = datetime.fromtimestamp(psutil.boot_time())
 
+            runtime = datetime.now() - create_time
+
             # get the status of the process (running, idle, etc.)
             try:
                 status = process.status()
@@ -47,11 +57,21 @@ def get_processes_info():
 
             # append process with information to process list
             processes.append({
-                'pid': pid, 'name': name, 'create_time': create_time
+                'pid': pid, 'name': name, 'create_time': create_time, 'runtime': runtime.total_seconds()
             })
 
     # return process list
     return processes
+
+def checkSystemProcess(name):
+    sysWin = ['alg.exe', 'csrss.exe', 'ctfmon.exe', 'explorer.exe', 'lsass.exe', 'services.exe', 'smss.exe', 'spoolsv.exe', 'svchost.exe', 'ntoskrnl.exe', 'winlogon.exe', 'System']
+
+    system = set(sysWin)
+    # if os.name == 'nt':
+    #    sys = set(sysWin)
+
+    if name in system:
+        return True
 
 
 # Constructs a dataframe out of the process list array
@@ -75,14 +95,16 @@ def processTest():
     print(df.to_string())
 
     # filter for all Edge processes
-    data = df.loc[df['name'] == "msedge.exe"]
-    print(data.to_string())
+    #data = df.loc[df['name'] == "msedge.exe"]
+    #print(data.to_string())
 
     # kill every Edge process
-    for i in data.index:
-        p = psutil.Process(i)
-        p.kill()
+    #for i in data.index:
+    #    p = psutil.Process(i)
+    #    p.kill()
 
+if __name__ == "__main__":
+    processTest()
 
 # Gives back the dataframe with all processes and information
 def getAllProcesses():
@@ -91,6 +113,54 @@ def getAllProcesses():
     return df
 
 class ProcessData():
-    def __init__(self, bannedProcesses):
+    def __init__(self, bannedProcesses=[]):
         self.data = getAllProcesses()
         self.bannedProcesses = bannedProcesses
+
+    def updateData(self):
+        self.data = getAllProcesses()
+
+    def checkProcesses(self):
+        processes = self.data['name']
+        running = []
+        proc = set(processes)
+        for b in self.banned:
+            for p in proc:
+                if b in p:
+                    running.append(b)
+
+        return running
+
+    def setBannedProcesses(self, bp):
+        if len(bp) != 0:
+            self.bannedProcesses = bp
+        else:
+            raise Exceptions.EmptyValueError
+
+    def clearBannedProcesses(self):
+        self.bannedProcesses.clear()
+
+    def getData(self):
+        return self.data
+
+    def getBannedProcesses(self):
+        return self.bannedProcesses
+
+    def extendBannedProcesses(self, name):
+        if isinstance(name, (list, tuple, np.ndarray)):
+            self.bannedProcesses.extend(name)
+        else:
+            self.bannedProcesses.append(name)
+
+    def removeBannedProcess(self, name):
+        if isinstance(name, (list, tuple, np.ndarray)):
+            for n in name:
+                try:
+                    self.bannedProcesses.remove(n)
+                except ValueError:
+                    raise Exceptions.NotFoundError
+        else:
+            try:
+                self.bannedProcesses.remove(name)
+            except ValueError:
+                raise Exceptions.NotFoundError
