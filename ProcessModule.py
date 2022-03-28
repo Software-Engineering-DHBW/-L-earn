3cff55cf40a6ac30f1b079c830113715ba438a91
Linus0815 <linus@engelundmueller.de>
2022-03-28 16:33:44 +0200

    gui changes to "Mitteilungen"

--- ProcessModule.py
+++ ProcessModule.py
@@ -149,7 +149,7 @@ def getAllProcesses():
     if platform == "win32":
         filterProcWin(df)
     elif platform == "linux":
-        filterProcWin(df)
+        filterProcLin(df)
     return df
 
 
