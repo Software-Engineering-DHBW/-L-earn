89ef0e11a98cfb81c472d9d377fae07122513278
reusdomi <66684261+reusdomi@users.noreply.github.com>
2022-03-29 12:40:59 +0200

    Update Worker.py

--- classes/Worker.py
+++ classes/Worker.py
@@ -64,7 +64,7 @@ class Worker(QObject):
                         i = 0
                         for r in running:
                             if not r in self.setTimers:
-                                self.timers.append(threading.Timer(300, self.timerEnds, [r]))
+                                self.timers.append(threading.Timer(300, self.timerEnds, [r, i]))
                                 self.setTimers.append(r)
                                 self.timers[i].start()
                                 title = "[L]earn - Limit Alert"
@@ -98,7 +98,7 @@ class Worker(QObject):
             time.sleep(300)
 
 
-    def timerEnds(self, proc):
+    def timerEnds(self, proc, i):
 
         try:
             pm.ProcessData().killProcess(proc)
